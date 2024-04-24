036d8202637d3905ca58cf2282fb3af37f35a90f
Krishna Anandan <krishna1729atom@gmail.com>
2024-04-24 17:14:09 -0500

    - Removed initial boilerplate and using basic CRA template

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,32 +1,12 @@
 import React from 'react';
-import {render} from 'react-dom';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import { Provider } from 'react-redux';
-import HeaderAppBar from './components/Header';
-import AppRouter from './routers/AppRouter';
-import './styles/styles.css';
-import {theme} from './theme/theme';
-import configureStore from './store/config/configureStore';
-import {login,logout} from './store/actions/auth';
+import Button from '@mui/material/Button';
 
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained">Hello MUI!</Button>
+    </div>
+  );
+}
 
-const store = configureStore();
-store.subscribe(()=>{
-  console.log(store.getState());
-});
-
-
-
-const App = () => (
-  <Provider store={store}>
-  <MuiThemeProvider theme={theme}>
-    <AppRouter />
-  </MuiThemeProvider>
-  </Provider>
-);
-
-
-store.dispatch(logout());
-
-
-render(<App />, document.getElementById('app'));
+export default App;
