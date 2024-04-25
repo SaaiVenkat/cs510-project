558ed633ed9297526412813a75452cbf3f6ce474
Anirudh Madhavan <anirudhragavender@gmail.com>
2024-04-24 20:43:41 -0500

    add new route bookmark/:id

--- ai-server/routes/document.py
+++ ai-server/routes/document.py
@@ -1,17 +1,16 @@
 from flask import Blueprint, request, jsonify
 from model import make_doc_to_embedding, index_with_faiss, cosine_similarity
 from weblink_parser import extract_web_link
-from pymongo import MongoClient, ReturnDocument
-import os
-import json
+from pymongo import ReturnDocument
 from threading import Thread, Lock
 from utils import MongoDB
+from models import Document
 
 document_bp = Blueprint("document", __name__)
 
 mongodb = MongoDB()
 db = mongodb.get_database("bert")
-collection = db["links"]
+document_collection = db["links"]
 counter_collection = db["counter"]
 
 # Ensure the counter collection has an initial document
@@ -34,7 +33,7 @@ def save_bookmark(bookmark):
     # taking content from the web link
     web_link_data = extract_web_link(bookmark)
     # Assign topics to bookmark content
-    topic_embeddings = make_doc_to_embedding(web_link_data["text_content"])
+    topic_embeddings = make_doc_to_embedding(web_link_data.text_content)
 
     # store this embedding in fiass
     index_with_faiss(topic_embeddings)
@@ -42,18 +41,19 @@ def save_bookmark(bookmark):
     link_id = get_next_sequence_value("link_id")
 
     # Store link data, embedding, and metadata in MongoDB
-    document = {
-        "_id": link_id,
-        "link": bookmark,
-        "embedding": topic_embeddings.tolist(),  # Convert NumPy array to Python list to store in mongo
-        "metadata": {
-            "title": web_link_data["title"],
-            "meta_tags": web_link_data["meta_tags"],
-            "links": web_link_data["links"],
-            "text_content": web_link_data["text_content"],
-        },
-    }
-    collection.insert_one(document)
+    document = Document(link_id, bookmark, topic_embeddings.tolist(), web_link_data)
+    # document = {
+    #     "_id": link_id,
+    #     "link": bookmark,
+    #     "embedding": topic_embeddings.tolist(),  # Convert NumPy array to Python list to store in mongo
+    #     "metadata": {
+    #         "title": web_link_data["title"],
+    #         "meta_tags": web_link_data["meta_tags"],
+    #         "links": web_link_data["links"],
+    #         "text_content": web_link_data["text_content"],
+    #     },
+    # }
+    document.add()
     return link_id
 
 
@@ -101,3 +101,19 @@ def process_string():
 
     link_id = save_bookmark(web_link)
     return jsonify({"success": True, "documentCount": link_id + 1}), 200
+
+
+# Route to find document by ID
+@document_bp.route("/bookmark/<string:id>", methods=["GET"])
+def get_document_by_id(id):
+    doc_data = Document.findByFaissId(id)
+    if doc_data:
+        ret = {
+            "id": str(doc_data._id),
+            "faiss_id": doc_data.faiss_id,
+            "url": doc_data.link,
+        }
+        print(ret)
+        return jsonify(ret), 200
+    else:
+        return jsonify({"message": "Document not found"}), 404
